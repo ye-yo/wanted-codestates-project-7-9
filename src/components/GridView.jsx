c96b7dacf995362e4797239396ee759f047f6929
ye-yo <yeyo0x0@gmail.com>
2022-03-10 20:49:08 +0900

    Design: image object-fit 설정 및 flex -> grid로 변경
    Related to #6

--- src/components/GridView.jsx
+++ src/components/GridView.jsx
@@ -8,7 +8,7 @@ function GridView() {
 
   return (
     <GridViewWrap>
-      {reviews.slice(0, 20).map((review) => (
+      {reviews.map((review) => (
         <ImageBox
           onClick={() => handleClickImage(review.id)}
           src={review.src}
@@ -22,32 +22,17 @@ export default GridView;
 
 const GridViewWrap = styled.section`
   width: 100%;
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 1px;
   margin-top: 2px;
 `;
 
 const ImageBox = styled.img`
-  max-width: calc((500px - 2px) / 3);
-  width: calc((500px - 2px) / 3);
+  width: 100%;
+  height: 100%;
   max-height: calc((500px - 2px) / 3);
-  border-top: 1px solid #fff;
-  border-left: 0;
-  border-right: 0;
-  &:nth-child(1),
-  &:nth-child(2),
-  &:nth-child(3) {
-    border-top: 0;
-  }
-  &:nth-child(3n - 1) {
-    border-left: 1px solid #fff;
-    border-right: 1px solid #fff;
-  }
-
   @media only screen and (max-width: 500px) {
-    max-width: calc((100vw - 2px) / 3);
-    width: calc((100vw - 2px) / 3);
     max-height: calc((100vw - 2px) / 3);
   }
 `;
